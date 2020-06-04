16ace01b93550762169700610b390540e281e62e
XuanNghiemNguyen <xuanghjem@gmail.com>
2020-06-04 07:45:56 +0700

    3 test internal:

---
+++ Test Suites/1612427/09_ResetUserEmail.ts
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>09_ResetUserEmail</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>2545a620-5917-4f86-80b5-b26016b93638</testSuiteGuid>
+   <testCaseLink>
+      <guid>f4ec4da0-dd13-4cf5-a29d-fa166e6591fc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1612427/09_ChangeUserEmail</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>fbfbe863-7483-4924-abf7-9701c1ec9a46</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/1612427/09_ChangeEmailUser</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>fbfbe863-7483-4924-abf7-9701c1ec9a46</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>newEmail</value>
+         <variableId>e8f022a9-065c-4f7e-b6d4-cc5f9ee1cbd5</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
